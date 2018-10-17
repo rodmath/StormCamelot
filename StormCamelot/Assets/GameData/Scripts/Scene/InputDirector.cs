a478b1fd13d09eda58bf17e165c7629f06f6ccb5
RodMath <rod.mathisen@mathisens.co.uk>
2018-10-17 21:27:44 +0100

    Time control, seperating input from objects

---
+++ StormCamelot/Assets/GameData/Scripts/Scene/InputDirector.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InputDirector : MonoBehaviour
+{
+    public float soldierSelectionRange = 4f;
+
+    private List<SoldierAgent> soldiers;
+    private SoldierAgent soldierSelected;
+    private SmoothCamera2D smoothCamera;
+
+    private void Start()
+    {
+        soldiers = Object.FindObjectsOfType<SoldierAgent>().ToList();
+        smoothCamera = Object.FindObjectOfType<SmoothCamera2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartClick();
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            if (soldierSelected)
+                UpdateSoldierControl();
+            else
+                UpdateCameraPanControl();
+        }
+    }
+
+    private void StartClick()
+    {
+        ////Find out where we have clicked, then test to see the nearest Soldier
+        Plane plane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // plane.Raycast returns the distance from the ray start to the hit point
+        float rayDist;
+        float dist;
+        float minDist = float.PositiveInfinity;
+        if (plane.Raycast(ray, out rayDist))
+        {
+            Vector3 clickPos = ray.GetPoint(rayDist);
+            foreach (SoldierAgent soldier in soldiers)
+            {
+                dist = (clickPos - soldier.transform.position).magnitude;
+                if (dist < soldierSelectionRange)
+                {
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        soldierSelected = soldier;
+                        Debug.Log("Soldier selected: " + soldier.name);
+                        smoothCamera.target = soldier.transform;
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void UpdateSoldierControl()
+    {
+
+    }
+
+    private void UpdateCameraPanControl()
+    {
+    }
+}
---
+++ StormCamelot/Assets/GameData/Scripts/Scene/SoldierAgent.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vectrosity;
+
+
+public class SoldierAgent : MonoBehaviour
+{
+    private CharacterController cc;
+
+
+    public bool takeInput = false;
+    public bool showDebug = true;
+
+    //[Header("Input stuff")]
+    private VectorLine aimMinLine;
+    private VectorLine aimMaxLine;
+    private VectorLine moveTriggerLine;
+
+    [Header("Aim stuff")]
+    public Vector2 aimVector = new Vector2(1, 0);
+    public Color32 aimColour = Color.black;
+    public float aimWidth = 2f;
+
+
+    [Header("Mouse distance limits")]
+    public float aimMin = 2f;
+    public float aimMax = 3f;
+    public float moveTriggerRange = 4f;
+
+    private VectorLine aimLine;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+
+        aimMinLine = new VectorLine("Min aim limit line", new List<Vector3>(64), 1);
+        aimMinLine.color = Color.blue;
+        aimMinLine.Draw3DAuto();
+
+        aimMaxLine = new VectorLine("Max aim limit line", new List<Vector3>(64), 1);
+        aimMaxLine.color = Color.blue;
+        aimMaxLine.Draw3DAuto();
+
+
+        moveTriggerLine = new VectorLine("Move trigger line", new List<Vector3>(64), 1);
+        moveTriggerLine.color = Color.blue;
+        moveTriggerLine.Draw3DAuto();
+
+
+        aimLine = new VectorLine("Aim line", new List<Vector3>(), aimWidth);
+        aimLine.points3.Add(transform.position);
+        aimLine.points3.Add(transform.position);
+
+        aimLine.color = aimColour;
+        aimLine.Draw3DAuto();
+
+
+    }
+
+
+    void Update()
+    {
+        //if (takeInput)
+        //    ProcessInput();
+        //else
+            //cc.Move(Useful.RandomVector2Direction() * Time.deltaTime);
+    }
+
+
+    private void ProcessInput()
+    {
+        if (showDebug)
+        {
+            aimMinLine.MakeCircle(transform.position, transform.up, aimMin);
+            aimMaxLine.MakeCircle(transform.position, transform.up, aimMax);
+            moveTriggerLine.MakeCircle(transform.position, transform.up, moveTriggerRange);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //get the distance from the mouse click to this transform
+            // this creates a horizontal plane passing through this object's center
+            Plane plane = new Plane(Vector3.up, transform.position);
+            // create a ray from the mousePosition
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // plane.Raycast returns the distance from the ray start to the hit point
+            float distance;
+            if (plane.Raycast(ray, out distance))
+            {
+
+                // some point of the plane was hit - get its coordinates
+                Vector3 hitpoint = ray.GetPoint(distance);
+
+                // figure out based on the input where we want to aim
+                Vector3 aimP = transform.position - hitpoint;
+                transform.rotation = Quaternion.LookRotation(aimP);
+
+                //if less than aim distance, just look
+                if (aimP.magnitude < aimMin)
+                    aimLine.color = Color.clear;
+
+                //if in aim area, proportionally put point
+                else if (aimP.magnitude < aimMax)
+                    aimLine.color = Color.white;
+
+                //if in move max aim area, put max aim point
+                else if (aimP.magnitude < moveTriggerRange)
+                    aimP = aimP.normalized * aimMax;
+
+                //if beyon move range; move
+                else
+                {
+                    aimLine.color = Color.clear;
+                    cc.Move(aimP * Time.deltaTime);
+                }
+
+
+
+                aimLine.points3[0] = transform.position;
+                aimLine.points3[1] = transform.position + aimP;
+            }
+        }
+
+    }
+}
