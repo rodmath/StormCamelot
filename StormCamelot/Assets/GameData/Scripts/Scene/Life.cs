2e7a9766a051d45a41bc84511f4e7f7e6d4800b8
RodMath <rod.mathisen@mathisens.co.uk>
2018-11-19 21:41:28 +0000

    Update Life.cs
    Added death throes and resurrection

--- StormCamelot/Assets/GameData/Scripts/Scene/Life.cs
+++ StormCamelot/Assets/GameData/Scripts/Scene/Life.cs
@@ -1,24 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Life : MonoBehaviour {
+public class Life : MonoBehaviour
+{
 
-    public bool alive = true;
-
-    private GameObject deadObject;
-
-    public void Dead()
+    private enum LifeState
     {
-        Debug.Log(name + " was killed");
-
-        //ragdollify
-        Rigidbody body = GetComponent<Rigidbody>();
-        if (body && body.isKinematic == true)
-            body.isKinematic = false;
-
-        alive = false;
+        alive,
+        takingLastBreaths,
+        dead
     }
 
+    public bool alive = true;
+    public float deathThrowDuration = 4f;
+
+    [SerializeField]
+    private LifeState state = LifeState.alive;
+    private bool cacheIsKinetic;
+    private Vector3 cachePosition;
+    private Quaternion cacheRotation;
+
+    public void Kill()
+    {
+        Debug.Log(name + " was killed");
+        alive = false;
+
+    }
+
+    private void Update()
+    {
+        if (!alive && state == LifeState.alive)
+            StartCoroutine(TakeLastBreaths());
+
+        if (alive && state == LifeState.dead)
+            StartCoroutine(Resurrect());
+    }
+
+
+    private IEnumerator TakeLastBreaths()
+    {
+        state = LifeState.takingLastBreaths;
+
+        NavMeshAgent nav = GetComponent<NavMeshAgent>();
+        if (nav)
+            nav.enabled = false;
+
+        Agent agent = GetComponent<Agent>();
+        if (agent)
+            agent.enabled = false;
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body)
+        {
+            //ragdollify
+            body.WakeUp();
+            cacheIsKinetic = body.isKinematic;
+            cachePosition = body.position;
+            cacheRotation = body.rotation;
+
+            if (body.isKinematic)
+                body.isKinematic = false;
+
+            //twitch
+            int twitches = Random.Range(2, 5);
+            for (int i = 0; i < twitches; i++)
+            {
+                body.AddForce(Random.onUnitSphere * body.mass, ForceMode.Impulse);
+                body.AddTorque(Random.onUnitSphere * body.mass, ForceMode.Impulse);
+
+                yield return new WaitForSeconds(deathThrowDuration / (float)twitches);
+            }
+
+            //fall through floor
+            foreach (Collider c in GetComponentsInChildren<Collider>())
+                c.enabled = false;
+        }
+
+        state = LifeState.dead;
+    }
+
+
+    private IEnumerator Resurrect()
+    {
+        state = LifeState.alive;
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body)
+        {
+            body.isKinematic = cacheIsKinetic;
+            body.MovePosition(cachePosition);
+            body.MoveRotation(cacheRotation);
+
+        }
+
+        //reactivate colliders
+        foreach (Collider c in GetComponentsInChildren<Collider>())
+            c.enabled = true;
+
+        //wait a few frames for our position to update before re-enabling navmesh agent (otherwise it's not created)
+        yield return new WaitForFixedUpdate();
+
+        NavMeshAgent nav = GetComponent<NavMeshAgent>();
+        if (nav)
+            nav.enabled = true;
+
+        Agent agent = GetComponent<Agent>();
+        if (agent)
+            agent.enabled = true;
+    }
 
 }
